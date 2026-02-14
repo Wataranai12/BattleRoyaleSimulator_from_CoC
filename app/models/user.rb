e2c749020a1282e34c191a0c8624786b73ea86c4
Wataranai12 <kumasaru2000@gmail.com>
2026-02-14 16:30:36 +0900

    fix:bin/devの追加、修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+
+  # パスワードは新規作成か変更時のみ、3文字以上必要
+  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
+  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
+
+  # メールアドレスとユーザー名は必須かつ一意
+  validates :email, uniqueness: true, presence: true
+  validates :name, presence: true
 end
